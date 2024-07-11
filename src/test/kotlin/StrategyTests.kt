5f9b48a15617b68ce97cbcef073e947ba65cbdc6
root <root@FinchComp>
2024-07-11 11:16:11 -0600

    created test

--- src/test/kotlin/StrategyTests.kt
+++ src/test/kotlin/StrategyTests.kt
@@ -3,6 +3,7 @@ import org.junit.Test
 import updateStrategies.CanceledUpdateStrategy
 import Shipment
 import ShippingUpdate
+import updateStrategies.CreatedUpdateStrategy
 
 class CanceledUpdateStrategyTest {
 
@@ -27,22 +28,24 @@ class CanceledUpdateStrategyTest {
     }
 
     @Test
-    fun `test CanceledUpdateStrategy handles update correctly`() {
+    fun `test CreatedUpdateStrategy handles update correctly`() {
         // Create necessary objects
         val shipment = Shipment("ABC123")
-        val update = ShippingUpdate("canceled", "ABC123", 1234)
+        val update = ShippingUpdate("created", "ABC123", 1234)
 
         // Create instance of CanceledUpdateStrategy
-        val strategy = CanceledUpdateStrategy()
+        val strategy = CreatedUpdateStrategy()
 
         // Apply strategy to handle update
         strategy.handleUpdate(shipment, update)
 
         // Assert that shipment status is updated correctly
-        assertEquals("canceled", shipment.status)
+        assertEquals("created", shipment.status)
 
         // Assert that update is added to shipment
         assertEquals(1, shipment.updateHistory.size)
 
     }
+
+    
 }
